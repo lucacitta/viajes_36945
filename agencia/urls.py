2fb61a8ca65fe8a321b3d73992d9225ccd65636c
luca citta <lucacitta@gmail.com>
2022-05-31 18:37:42 -0300

    changes

--- agencia/urls.py
+++ agencia/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from agencia.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('viajes/', include('viajes.urls'))
+    path('viajes/', include('viajes.urls')),
+    path('', index, name = 'index')
 ]
